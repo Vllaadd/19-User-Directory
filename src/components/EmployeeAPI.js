84651164b0673245988f66ff9ac85225984ef4ec
Vladimir Zizic <vlladdiimiir@gmail.com>
2020-12-14 05:42:14 -0500

    app-employeeAPI-EmployeeList-Employee is the hirerachy

--- src/components/EmployeeAPI.js
+++ src/components/EmployeeAPI.js
@@ -1,3 +1,13 @@
-import React from 'react';
+import React, { Component } from 'react';
 import EmployeeList from './EmployeeList/index';
 
+class EmployeeAPI extends Component {
+    state = {  }
+    render() { 
+        return ( 
+            <EmployeeList />
+         );
+    }
+}
+ 
+export default EmployeeAPI;
